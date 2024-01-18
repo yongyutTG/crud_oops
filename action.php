82628e1f94da7f68a1fe8203015174a8fd06ee51
YONGYUT <78497837+yongyutTG@users.noreply.github.com>
2024-01-18 15:04:43 +0700

    Add files via upload

---
+++ action.php
@@ -0,0 +1,117 @@
+<?php
+   // Include config.php file
+   include_once('config.php');
+
+   $dbObj = new Database();
+
+   // Insert Record   
+   if (isset($_POST['action']) && $_POST['action'] == "insert") {
+
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $username = $_POST['username'];
+      $dob = $_POST['dob'];
+      $dbObj->insertRecond($name, $email, $username, $dob);
+   }
+
+   // View record
+   if (isset($_POST['action']) && $_POST['action'] == "view") {
+      $output = "";
+
+      $customers = $dbObj->displayRecord();
+
+      if ($dbObj->totalRowCount() > 0) {
+         $output .="<div class='table-responsive-sm'>
+         <table class='table '>   
+         
+                 <thead class='thead-dark'>
+                   <tr>
+                     <th>Id</th>
+                     <th>Name</th>
+                     <th>Email</th>
+                     <th>Username</th>
+                     <th>Date of birth</th>
+                     <th>Action</th>
+                   </tr>
+                 </thead>
+                 <tbody>";
+         foreach ($customers as $customer) {
+         $output.="<tr>
+                     <td>".$customer['id']."</td>
+                     <td>".$customer['name']."</td>
+                     <td>".$customer['email']."</td>
+                     <td>".$customer['username']."</td>
+                     <td>".date('d-M-Y', strtotime($customer['dob']))."</td>
+                     <td>
+                       <a href='#editModal' style='color:green' data-toggle='modal' 
+                       class='editBtn' id='".$customer['id']."'><i class='fa fa-pencil'></i></a>&nbsp;
+                       <a href='' style='color:red' class='deleteBtn' id='".$customer['id']."'>
+                       <i class='fa fa-trash' ></i></a>
+                     </td>
+                 </tr>";
+            }
+         $output .= "</tbody>
+            </table>";
+            echo $output;   
+      }else{
+         echo '<h3 class="text-center mt-5">No records found</h3>';
+      }
+   }
+
+   // Edit Record   
+   if (isset($_POST['editId'])) {
+      $editId = $_POST['editId'];
+      $row = $dbObj->getRecordById($editId);
+      echo json_encode($row);
+   }
+
+
+   // Update Record
+   if (isset($_POST['action']) && $_POST['action'] == "update") {
+      $id = $_POST['id'];
+      $name = $_POST['uname'];
+      $email = $_POST['uemail'];
+      $username = $_POST['uusername'];
+      $dob = $_POST['udob'];
+      $dbObj->updateRecord($id, $name, $email, $username, $dob);
+   }
+
+
+   // Edit Record   
+   if (isset($_POST['deleteBtn'])) {
+      $deleteBtn = $_POST['deleteBtn'];
+      $dbObj->deleteRecord($deleteBtn);
+   }
+
+
+   // Export to excel
+   if (isset($_GET['export']) && $_GET['export'] == 'excel') {
+
+      header("Content-type: application/vnd.ms-excel; name='excel'");
+      header("Content-Disposition: attachment; filename=customers.xls");
+      header("Pragma: no-cache");
+      header("Expires: 0");
+
+      $exportData = $dbObj->displayRecord();
+
+      echo'<table border="1">
+         <tr style="font-weight:bold">
+             <td>Id</td>
+             <td>Name</td>
+             <td>Email</td>
+             <td>Username</td>
+             <td>Dob</td>
+         </tr>';
+      foreach ($exportData as $export) {
+      echo'<tr>
+         <td>'.$export['id'].'</td>
+         <td>'.$export['name'].'</td>
+         <td>'.$export['email'].'</td>
+         <td>'.$export['username'].'</td>
+         <td>'.date('d-M-Y', strtotime($export['dob'])).'</td>
+           </tr>';
+         }      
+      echo '</table>';
+   }
+
+?>
---
+++ config.php
@@ -0,0 +1,97 @@
+<?php
+  
+  class Database 
+  {
+    private $servername = "localhost";
+    private $username   = "root";
+    private $password   = "1234";
+    private $dbname = "yongyut";
+    public $con;
+    public $customerTable = "customers";
+
+    public function __construct()
+    {
+      try {
+        $this->con = new mysqli($this->servername, $this->username, $this->password, $this->dbname);  
+      } catch (Exception $e) {
+        echo $e->getMessage();
+      }
+    }
+
+    // Insert customer data into customer table
+    public function insertRecond($name, $email, $username, $dob)
+    {
+      $sql = "INSERT INTO $this->customerTable (name, email, username, dob) VALUES('$name','$email','$username','$dob')";
+      $query = $this->con->query($sql);
+      if ($query) {
+        return true;
+      }else{
+        return false;
+      }
+    }
+
+    // Fetch customer records for show listing
+    public function displayRecord()
+    {
+      $sql = "SELECT * FROM $this->customerTable";
+      $query = $this->con->query($sql);
+      $data = array();
+      if ($query->num_rows > 0) {
+        while ($row = $query->fetch_assoc()) {
+          $data[] = $row;
+        }
+        return $data;
+      }else{
+        return false;
+      }
+    }
+
+    // Fetch single data for edit from customer table
+    public function getRecordById($id)
+    {
+      $query = "SELECT * FROM $this->customerTable WHERE id = '$id'";
+      $result = $this->con->query($query);
+      if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        return $row;
+      }else{
+        return false;
+      }
+    }
+
+
+    public function totalRowCount(){
+      $sql = "SELECT * FROM $this->customerTable";
+      $query = $this->con->query($sql);
+      $rowCount = $query->num_rows;
+      return $rowCount;
+    }
+  
+
+    // Update customer data into customer table
+    public function updateRecord($id, $name, $email, $username, $dob)
+    {
+      $sql = "UPDATE $this->customerTable SET name = '$name', email = '$email', username = '$username', dob = '$dob' 
+      WHERE id = '$id'";
+      $query = $this->con->query($sql);
+      if ($query) {
+        return true;
+      }else{
+        return false;
+      }
+    }
+
+    // Delete customer data from customer table
+    public function deleteRecord($id)
+    {
+      $sql = "DELETE FROM $this->customerTable WHERE id = '$id'";
+      $query = $this->con->query($sql);
+      if ($query) {
+        return true;
+      }else{
+        return false;
+      }
+    }
+  }
+
+?>
